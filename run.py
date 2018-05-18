a9271b2cc7fca5b84c7a29dad7e54868490c0c8d
michaelkunc <mjkunc@gmail.com>
2018-05-18 07:03:47 -0700

    bootstrapped!

---
+++ run.py
@@ -0,0 +1,14 @@
+
+# TODO split everything out into their own files later
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
